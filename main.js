175d5efd9879ada5f49ea7e550cdefcc14072cd6
Waters <894394616@qq.com>
2020-05-21 14:30:21 +0800

    v4 - 完成八卦图

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 let html = document.querySelector("#html"); //获取#html
 let style = document.querySelector("#style"); //获取#style
 
-let string = `/*
+let string = `
+/*
 你好，我是一名前端小白
 今天要画的是一只叫八卦的大熊猫
 这就让你见识一下我的前端功底 (っ•̀ω•́)っ✎⁾⁾
 */
-body{
-    color: red;
+
+/*首先，画一个正方形*/
+#div1 {
+    height: 400px;
+    width: 400px;
+    border: 2px solid grey;
+  }  
+  
+ /*看我把它变胖，变成熊猫色*/
+#div1 {
+    border-radius: 50%;
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(7,14,23,1) 100%);
 }
+
+/*再让它长出两只熊猫耳朵*/
+#div1::before,#div1::after {
+    border: 1px solid grey;
+    border-radius: 50%;
+}
+#div1::before {
+    background-color: #fff;
+}
+#div1::after {
+    background-color: #000;
+}
+
+/*再加两个黑眼圈*/
+#div1::before {
+    border:none;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 35%, rgba(255,255,255,1) 35%, rgba(255,255,255,1) 100%);
+}
+#div1::after{
+    border: none;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 35%, rgba(0,0,0,1) 35%, rgba(7,14,23,1) 100%);
+}
+/*完成！
+  什么，你说不像？
+  仔细看，它在睡觉zzZ*/
+  
 `;
 
 let string2 = ""; //为了空格回车
@@ -37,6 +74,6 @@ let step = () => {
       step(); //判断:如果n小于字符串总长度就重新运行
     } else {
     }
-  }, 100);
+  }, 0);
 };
 step();
